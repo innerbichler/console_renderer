b7788a2eac24dd9a12b18f08c08e74f4d604e029
innerbichler <alexinnerbichler43@gmail.com>
2023-09-22 23:20:46 +0200

    working rendering of fullscreen

--- main.cpp
+++ main.cpp
@@ -6,15 +6,26 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <cstdlib>
+#include <memory>
 
 
 
 class Pixel{
-	public:
-		char value;
-		std::string color;
-		Pixel(char value, std::string color){};
+public:
+	
+	char value;
+	std::string fg;
+	std::string bg;
 
+	Pixel(char v, std::string f, std::string b){
+		value = v;
+		fg = f;
+		bg = b;
+	};
+
+	std::string get_value(){
+		return fg + bg + value;
+	}
 };
 
 class Renderer{
@@ -23,64 +34,83 @@ public:
 	int windowHeight;
 	int windowWidth;
 
+	std::vector<std::vector<Pixel>> screen;
+
 	Renderer(){
 		struct winsize w;
 		ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 		windowWidth = w.ws_col;
 		windowHeight = w.ws_row;
-	}
-	virtual void onUpdate(int delay){};
-
-	void mainLoop(){
-		int delay = 1;
-		while (true){	// updateloop
-			onUpdate(delay);
-			sleep(1);
-		}
-	}
-
-	void renderScreen(std::vector<std::vector<Pixel>> screen){
-		std::cout << "\x1B[2J\x1B[H";
-
-		for (int y=0; y<Renderer::windowHeight; y++) {
-			for (int x=0; x<Renderer::windowHeight; x++) {
-				std::cout << screen[y][x].value;
-			}
-			std::cout << std::endl;
-		}
-	}
-};
-
-class Custom : public Renderer{
-	std::vector<std::vector<Pixel>> screen;
-public:
-	Custom() : Renderer(){}
-
-	void onCreate(){
-		//initialise variables
+		
+		// initalise screen
 		std::vector<Pixel> row;
-
 		for (int y=0; y<Renderer::windowHeight; y++) {
-			for (int x=0; x<Renderer::windowHeight; x++) {
-				//char letter = 97 + std::rand() % 122;
-				char letter = 'a';
-				std::string color = "WOW";
-				row.push_back(Pixel(letter, color));
+			for (int x=0; x<Renderer::windowWidth; x++) {
+				char letter = ' ';
+				std::string fg = "\u001b[37m";
+				std::string bg = "\u001b[40m";
+				row.push_back(Pixel(letter, fg, bg));
 			}
 			screen.push_back(row);
 			row.clear();
 		}
+//
 	}
 
-	void onUpdate(int delay){
+	virtual void onUpdate(int delay){};
+
+	void mainLoop(){
+		int delayms = 100;
+		while (true){	// updateloop
+			onUpdate(delayms);
+			renderScreen();
+			std::this_thread::sleep_for(std::chrono::milliseconds(delayms));
+		}
+	}
+	void renderScreen(){
+		for (auto y=screen.begin(); y!=screen.end(); ++y){
+			for (auto x= y->begin(); x!= y->end(); ++x) // this feels like black magic to me
+				std::cout << x->get_value();
+		}
+		std::cout << std::endl;
+		std::cout << "\x1B[2J\x1B[H";
+	}
+
+};
+
+class Custom : public Renderer{
+	int count = 0;
+public:
+	Custom() : Renderer(){}
+
+	void onUpdate(int delayms){
 		//game logic
-		Renderer::renderScreen(screen);
-		onCreate();
 
+		if (count % 2 == 0){
+			for (auto y=Renderer::screen.begin(); y!=Renderer::screen.end(); ++y){
+				for (auto x= y->begin(); x!= y->end(); ++x){
+					x->bg = "\u001b[41m";
+					x->fg = "\u001b[30m";
+					x->value = '?';
+				}
+			};
+		}
+		else{
+			for (auto y=Renderer::screen.begin(); y!=Renderer::screen.end(); ++y){
+				for (auto x= y->begin(); x!= y->end(); ++x){
+					x->bg = "\u001b[40m";
+					x->fg = "\u001b[31m";
+					x->value = '!';
+				}
+			};
+
+
+		}
+		++count;
 	}
+
 public:
 	void startConsole(){
-		onCreate();
 		Renderer::mainLoop();
 	}
 
@@ -88,10 +118,7 @@ public:
 
 int main(){
 
-
-	std::srand(std::time(0));
 	Custom myConsole;
 	myConsole.startConsole();
-
 }
 
