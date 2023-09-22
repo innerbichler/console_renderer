9f419973c95367181199e5fc32a56e6e6100bf18
innerbichler <alexinnerbichler43@gmail.com>
2023-09-23 01:02:29 +0200

    working add rect

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ public:
 			for (int x=0; x<Renderer::windowWidth; x++) {
 				char letter = ' ';
 				std::string fg = "\u001b[37m";
-				std::string bg = "\u001b[40m";
+				std::string bg = "\u001b[0m";
 				row.push_back(Pixel(letter, fg, bg));
 			}
 			screen.push_back(row);
@@ -60,7 +60,7 @@ public:
 	virtual void onUpdate(int delay){};
 
 	void mainLoop(){
-		int delayms = 100;
+		int delayms = 250;
 		while (true){	// updateloop
 			onUpdate(delayms);
 			renderScreen();
@@ -72,44 +72,53 @@ public:
 			for (auto x= y->begin(); x!= y->end(); ++x) // this feels like black magic to me
 				std::cout << x->get_value();
 		}
+		std::cout << "\x1b[?25l";	// disable cursor
 		std::cout << std::endl;
 		std::cout << "\x1B[2J\x1B[H";
+		}
+
+	void addRect(float x1, float y1, float x2, float y2, std::string color){
+		// wrapper around manipulating the screen vector directly
+		int newx1 = (windowWidth-1) * x1;
+		int newy1 = (windowHeight-1) * y1;
+		int newx2 = (windowWidth-1) * x2;
+		int newy2 = (windowHeight-1) * y2;
+
+		
+		for (int i = newy1; i <= newy2; ++i) {
+			for (int j = newx1; j <= newx2; ++j) {
+				screen[i][j].bg = color;
+			}
+			for (int j = newx1; j >= newx2; --j) {
+				screen[i][j].bg = color;
+			}
+		}
+
+		for (int i = newy1; i >= newy2; --i) {
+			for (int j = newx1; j >= newx2; --j) {
+				screen[i][j].bg = color;
+			}
+			for (int j = newx1; j <= newx2; ++j) {
+				screen[i][j].bg = color;
+			}
+		}
+
 	}
 
 };
 
 class Custom : public Renderer{
-	int count = 0;
 public:
+	int count = 0;
+
 	Custom() : Renderer(){}
 
 	void onUpdate(int delayms){
-		//game logic
-
-		if (count % 2 == 0){
-			for (auto y=Renderer::screen.begin(); y!=Renderer::screen.end(); ++y){
-				for (auto x= y->begin(); x!= y->end(); ++x){
-					x->bg = "\u001b[41m";
-					x->fg = "\u001b[30m";
-					x->value = '?';
-				}
-			};
-		}
-		else{
-			for (auto y=Renderer::screen.begin(); y!=Renderer::screen.end(); ++y){
-				for (auto x= y->begin(); x!= y->end(); ++x){
-					x->bg = "\u001b[40m";
-					x->fg = "\u001b[31m";
-					x->value = '!';
-				}
-			};
-
-
-		}
-		++count;
+		// runs on every 'frame'	
+		Renderer::addRect(0.125f, 0.5f, 1.0f, 0.5f, "\u001b[41m");
+		Renderer::addRect(0.125f, 1.0f, 0.5f, 1.0f, "\u001b[44m");
 	}
 
-public:
 	void startConsole(){
 		Renderer::mainLoop();
 	}
@@ -118,7 +127,7 @@ public:
 
 int main(){
 
-	Custom myConsole;
+	Custom myConsole = Custom();
 	myConsole.startConsole();
 }
 
