706d8606171532a944980bfdd8945f3ca75be436
alex wsl pc <alexinnerbichler43@gmail.com>
2023-09-24 11:59:43 +0200

    added input handler

--- main.cpp
+++ main.cpp
@@ -4,19 +4,22 @@
 class Custom : public cr::Renderer{
 	float count = 0.0f;
 
-	void onInput(std::string input){
+
+	void onInput(char input){
 		// handle user input
+		if (input == 'w'){
+			count = count - 0.03f;
+		}
+		if (input == 's'){
+			count = count + 0.03f;
+		}
+
 
 	}
 
 	void onUpdate(int delay){
 		// handle graphics and game logic
-		
-		if (count < 1.0f)
-			count = count + 0.001f;
-		if (count >= 1.0f)
-			count = 0.0f;
-
+		//
 		cr::Renderer::addRect(0.125f, 0.5f, 1.0f, 0.5f, cr::ConsoleColor(255,0,0));
 		cr::Renderer::addRect(0.125f, 0.55f, 1.0f, 0.55f, cr::ConsoleColor(128,255,255));
 		cr::Renderer::addRect(0.0f, count, 1.0f, count, cr::ConsoleColor(0,0,255));
