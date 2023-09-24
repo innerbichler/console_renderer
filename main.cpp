845c6bcb423b3589833908571759d121757c6e81
innerbichler <alexinnerbichler43@gmail.com>
2023-09-24 23:29:42 +0200

    moving balls

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <string>
+#include <random>
 #include "cr.h"
+
 
 class Custom : public cr::Renderer{
 
 	cr::Rect player = cr::Rect(cr::v2f(0.4f, 0.5f), cr::v2f(0.4f, 0.5f), cr::ConsoleColor(172,62,45));
+	cr::Rect block = cr::Rect(cr::v2f(0.2f, 0.5f), cr::v2f(0.2f, 0.5f), cr::ConsoleColor(172,62,45));
+	cr::v2f blockSpeed = cr::v2f(0.008f, 0.03f);
 
 	std::vector<cr::Rect> blocks;
+	std::vector<cr::Rect> balls;
+	std::vector<cr::v2f> ballSpeed;
+
 
 	float ySpeed = 0.03f;
 	float xSpeed = 0.008f;
@@ -19,11 +26,17 @@ class Custom : public cr::Renderer{
 		cr::Renderer::background.transparent = true;
 
 		// creating borders 
-		blocks.push_back(cr::Rect(cr::v2f(0.0f,0.0f), cr::v2f(0.0f,1.0f), cr::ConsoleColor(1,13,21)));
+		blocks.push_back(cr::Rect(cr::v2f(0.0f,0.0f), cr::v2f(0.01f,1.0f), cr::ConsoleColor(1,13,21)));
 		blocks.push_back(cr::Rect(cr::v2f(0.0f,0.0f), cr::v2f(1.0f,0.0f), cr::ConsoleColor(1,13,21)));
-		blocks.push_back(cr::Rect(cr::v2f(1.0f,0.0f), cr::v2f(1.0f,1.0f), cr::ConsoleColor(1,13,21)));
-		blocks.push_back(cr::Rect(cr::v2f(0.0f,1.0f), cr::v2f(1.0f,1.0f), cr::ConsoleColor(1,13,21)));
+		blocks.push_back(cr::Rect(cr::v2f(1.0f,0.0f), cr::v2f(0.99f,1.0f), cr::ConsoleColor(1,13,21)));
+		blocks.push_back(cr::Rect(cr::v2f(0.0f,0.999f), cr::v2f(1.0f,1.0f), cr::ConsoleColor(1,13,21)));
+
 		blocks.push_back(cr::Rect(cr::v2f(0.5f, 0.4f), cr::v2f(0.6f, 0.6f), cr::ConsoleColor(1,13,21)));
+		
+		balls.push_back(cr::Rect(cr::v2f(0.3f, 0.4f), cr::v2f(0.3f, 0.4f), cr::ConsoleColor(172,62,45)));
+		ballSpeed.push_back(blockSpeed);
+		balls.push_back(cr::Rect(cr::v2f(0.2f, 0.4f), cr::v2f(0.2f, 0.4f), cr::ConsoleColor(172,62,45)));
+		ballSpeed.push_back(blockSpeed);
 	}
 	bool playerCollision(){
 
@@ -88,13 +101,47 @@ class Custom : public cr::Renderer{
 		for (auto rect : blocks){
 			cr::Renderer::drawRect(rect.start, rect.end, rect.color);
 		}
+
+		int ballCount = 0;
+		for (auto ball=balls.begin(); ball!=balls.end(); ++ball){
+			
+			ball->color = cr::ConsoleColor(172,62,42);
+
+			ball->start.x += ballSpeed[ballCount].x;
+			ball->start.y += ballSpeed[ballCount].y;
+			ball->end.x += ballSpeed[ballCount].x;
+			ball->end.y += ballSpeed[ballCount].y;
+			
+			for (auto rect : blocks){
+				cr::Rect test = *ball;
+
+				if (cr::Renderer::rectCollision(*ball, rect) || cr::Renderer::rectCollision(*ball, player)){
+					cr::Renderer::drawText(cr::v2f(0.5f,0.5f), "Debug: Collision", mainFg);
+					ball->color = cr::ConsoleColor(255,255,255);
+
+					ballSpeed[ballCount].x = -ballSpeed[ballCount].x;
+					ballSpeed[ballCount].y = -ballSpeed[ballCount].y;
+				}
+			}
+
+			++ballCount;
+
+		}
+
+		for (auto ball : balls)
+			cr::Renderer::drawRect(ball.start, ball.end, ball.color);
+
+
 		cr::Renderer::drawText(cr::v2f(0.5f,1.0f), lastInput, mainFg);
 		cr::Renderer::drawText(cr::v2f(0.2f,1.0f), "Time between: " + std::to_string(delay)+"ms", mainFg);
+		cr::Renderer::drawText(cr::v2f(0.7f,1.0f), "Player: " + std::to_string(player.start.x) +std::to_string(player.start.y), mainFg);
 		cr::Renderer::drawRect(player.start, player.end, player.color);
+		
 	};
 };
 
 int main(){
+	std::srand(time(0));
 	Custom app;
 	app.run();
 }
